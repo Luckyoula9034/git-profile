cc0b5e5781dc1764e58a715fa1288bc9f12d1fca
luqqy <luckyoula@gmail.com>
2020-04-14 15:29:35 +0300

    added boostrap

--- services/profile.service.ts
+++ services/profile.service.ts
@@ -9,7 +9,7 @@ export class ProfileService {
    private clientsecret='3298fc43941620572fa5c506c3cc977e0c3d561b';
   constructor(private http:HttpClient) { 
     console.log("service is ready")
-    this.username = 'Luckyoula9034'
+    this.username = 'luckyoula9034'
   }
   getProfileInfo(){
     return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)};
